b93d9eda4f1e54e6c3c32f877ceaff4cdcbbd3be
Roy Marquez <theroyal3655@gmail.com>
2014-10-03 13:44:04 -0700

    Snake
    Adding more to my snake.

---
+++ public_html/js/Snake.js
@@ -0,0 +1,30 @@
+var snake;
+var snakeLength;
+var snakeSize;
+
+var context;
+var screenWidth;
+var screenHeight;
+
+gameInitialize();
+gameDraw();
+
+function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+}
+    
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+   context.fillStyle =  "rgb(108, 245, 181)";
+   context.fillRect(0, 0, screenWidth, screenHeight);
+}
