6545e4a6054c68013b95d95b00219560ca93473a
Roy Marquez <theroyal3655@gmail.com>
2014-10-10 13:50:07 -0700

    Adding the new vids.
    I learned how to change the snakes direction

--- public_html/js/Snake.js
+++ public_html/js/Snake.js
@@ -1,21 +1,19 @@
 /*
+ * >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
  * My variables
+ * <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
  */
 
 var snake;
 var snakeLength;
 var snakeSize;
+var snakeDirection;
 
 var food;
 
 var context;
 var screenWidth;
 var screenHeight;
-
-/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
- * 
- *%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
- */
 
 gameInitialize();
 snakeInitialize();
@@ -42,7 +40,7 @@ function gameLoop() {
 }
 
 function gameDraw() {
-   context.fillStyle =  "rgb(108, 245, 181)";
+   context.fillStyle =  "rgb(84, 28, 11)";
    context.fillRect(0, 0, screenWidth, screenHeight);
 }
 
@@ -50,6 +48,7 @@ function snakeInitialize() {
     snake = [];
     snakeLength = 5;
     snakeSize = 20;
+    snakeDirection = "right";
     
     for(var index = snakeLength - 1; index >= 0; index--) {
         snake.push({
@@ -61,7 +60,7 @@ function snakeInitialize() {
 
 function snakeDraw() {
     for(var index = 0; index < snake.length; index++) {
-        context.fillStyle = "Green";
+        context.fillStyle = "rgb(152, 224, 43)";
         context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
     }
 }
@@ -70,7 +69,9 @@ function snakeUpdate() {
     var snakeHeadX = snake[0].x;
     var snakeHeadY = snake[0].y;
     
-    snakeHeadX++;
+    if(snakeDirection == "right") {
+        snakeHeadX++;
+    }
     
     var snakeTail = snake.pop();
     snakeTail.x = snakeHeadX;
@@ -90,7 +91,11 @@ function foodDraw() {
     context.fillStyle = "Brown";
     context.fillRect(food.x, food.y, snakeSize, snakeSize);
 }
-
+/*
+ * >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ * This code randomly places the food
+ * <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+ */
 function setFoodPosition() {
     var randomX = Math.floor(Math.random() * screenWidth);
     var randomY = Math.floor(Math.random() * screenHeight);
